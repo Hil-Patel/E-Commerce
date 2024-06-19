766ed252e9067a84bf6f05f1597fbd94470d7cef
HIL PATEL <145318878+Hil-Patel@users.noreply.github.com>
2024-06-19 17:19:06 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,2 @@
 import React, { useState } from "react";
-import Navbar from "../components/Navbar";
-import LogInForm from "../components/LogInForm";
-
-const Login = () => {
-  const [loggedInStatus, setloggedInStatus] = useState(false);
-  
-
-  const changeLogStatus =(value)=>{
-    setloggedInStatus(value);
-  }
-
-  return (
-    <>
-      <Navbar loggedInStatus={loggedInStatus} />
-      <LogInForm changeLogStatus={changeLogStatus}/>
-    </>
-  );
-};
-
-export default Login;
+import Navbar from "../components/Navbar";import LogInForm from "../components/LogInForm";const Login = () => {  const [loggedInStatus, setloggedInStatus] = useState(false);    const changeLogStatus =(value)=>{    setloggedInStatus(value);  }  return (    <>      <Navbar loggedInStatus={loggedInStatus} />      <div className="fixed flex flex-col bg-gray-100 p-3 right-5 mt-5 rounded-md">        <div>username : mor_2314</div>        <div>password : 83r5^_</div>      </div>      <LogInForm changeLogStatus={ changeLogStatus } />    </>  );};export default Login;
